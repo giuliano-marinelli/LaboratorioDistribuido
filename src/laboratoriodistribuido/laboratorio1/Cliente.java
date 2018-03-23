7e010d4fde79e2cce906e11d59934f6428c14082
Martinnqn <bermudez_martin.nqn@hotmail.com>
2018-03-23 17:24:00 -0300

    branchea2. ahora cada barco busca su mejor movimiento

--- src/laboratoriodistribuido/laboratorio1/Cliente.java
+++ src/laboratoriodistribuido/laboratorio1/Cliente.java
@@ -7,7 +7,6 @@ public class Cliente {
 
     private final static int CENTRAL_PORT = 10000;
     private final static String SERVER = "localhost";
-    private int AAAAA;
 
     public static void main(String[] args) throws IOException {
         try {
